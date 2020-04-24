554036ca2807ef20f547710fdfecb5ec946f6c94
Juan Carlos Hoyos <jcabarique@uninorte.edu.co>
2020-04-23 20:28:37 -0500

    Se inicio el backend de MAC

---
+++ src/routes/login.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+require('./database');
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+//routes
+app.use(require('./routes/index'));
+
+
+
+app.listen(3000, ()=>{
+    console.log("servidor conectado en el puerto 3000")
+});
